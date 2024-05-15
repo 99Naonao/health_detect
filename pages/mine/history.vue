73928bdd4b66206cc2c35ac5fcd55434e379200d
colorfulee <54055007@qq.com>
2024-05-15 23:39:34 +0800

    历史记录

--- pages/mine/history.vue
+++ pages/mine/history.vue
@@ -1,5 +1,13 @@
 <template>
 	<view class="">
+		<view class="flex just-align-center align-center" style="justify-content: space-around;">
+			<view :class="active==0?'item active':'item'" @click="itemChange(0)">
+				周
+			</view>
+			<view :class="active==1?'item active':'item'" @click="itemChange(1)">
+				月
+			</view>
+		</view>
 		<serious-echart :icon="'icon-heart'" :title="'心率'"></serious-echart>
 		<serious-echart :icon="'icon-press'" :title="'血压'"></serious-echart>
 	</view>
@@ -15,7 +23,9 @@
 			seriousEchart
 		},
 		data() {
-			return {}
+			return {
+				active: 0
+			}
 		},
 		onShow() {
 			getHistoryData({
@@ -26,10 +36,28 @@
 		methods: {
 			init() {
 
+			},
+			itemChange(index) {
+				this.active = index;
 			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+	.item {
+		background-color: #eee;
+		color: #333;
+		font-size: 40rpx;
+		padding: 20rpx;
+		margin: 10rpx;
+		padding-left: 30rpx;
+		padding-right: 30rpx;
+		border-radius: 30rpx;
+	}
+
+	.active {
+		background-color: #5a7795;
+		color: white;
+	}
 </style>
